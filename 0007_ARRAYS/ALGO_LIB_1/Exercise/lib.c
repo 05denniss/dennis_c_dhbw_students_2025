119e05552f398e7b906abfa582dd37ab5853c2cc
05denniss <nik.dennis.05@gmail.com>
2026-01-08 18:37:38 +0100

    algo_lib_1_lösung

--- 0007_ARRAYS/ALGO_LIB_1/Exercise/lib.c
+++ 0007_ARRAYS/ALGO_LIB_1/Exercise/lib.c
@@ -5,16 +5,53 @@
 
 bool allOf(int32_t *array, size_t length, int32_t value)
 {
+    if (array == NULL) return false;
+
+    for (size_t i = 0; i < length; i++) {
+
+        if (array[i] != value) {
+            return false; 
+        }
+    }
+    return true;
 }
 
 bool anyOf(int32_t *array, size_t length, int32_t value)
 {
+    if (array == NULL) return false;
+
+    for (size_t i = 0; i < length; i++) {
+
+        if (array[i] == value) {
+            return true; 
+        }
+    }
+    return false;
 }
 
 bool noneOf(int32_t *array, size_t length, int32_t value)
 {
+    if (array == NULL) return false;
+
+    for (size_t i = 0; i < length; i++) {
+
+        if (array[i] == value) {
+            return false; 
+        }
+    }
+    return true;
 }
 
 size_t count(int32_t *array, size_t length, int32_t value)
 {
+    if (array == NULL) return false;
+
+    size_t count = 0;
+    for (size_t i = 0; i < length; i++) {
+
+        if (array[i] == value) {
+            count++; 
+        }
+    }
+    return count;
 }
